a02c9398bc426d4dee08d546de04507dc7ce21c7
Silas_Zahner <silas.zahner@gmail.com>
2023-06-19 11:44:47 +0000

    saving bmi result to firestore added

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'login_page.dart';
 
 enum BMICategory {
@@ -16,25 +17,26 @@ enum BMICategory {
 class ResultsPage extends StatelessWidget {
   final double bmi;
 
-  ResultsPage({required this.bmi});
+  const ResultsPage({super.key, required this.bmi});
 
   BMICategory getBMICategory(double bmi) {
-    if (bmi < 16.0)
+    if (bmi < 16.0) {
       return BMICategory.severeUnderweight;
-    else if (bmi < 17.0)
+    } else if (bmi < 17.0) {
       return BMICategory.moderateUnderweight;
-    else if (bmi < 18.5)
+    } else if (bmi < 18.5) {
       return BMICategory.slightUnderweight;
-    else if (bmi < 25.0)
+    } else if (bmi < 25.0) {
       return BMICategory.normal;
-    else if (bmi < 30.0)
+    } else if (bmi < 30.0) {
       return BMICategory.overweight;
-    else if (bmi < 35.0)
+    } else if (bmi < 35.0) {
       return BMICategory.obesityClassI;
-    else if (bmi < 40.0)
+    } else if (bmi < 40.0) {
       return BMICategory.obesityClassII;
-    else
+    } else {
       return BMICategory.obesityClassIII;
+    }
   }
 
   Map<String, String> getBMICategories() {
@@ -85,13 +87,13 @@ class ResultsPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR RESULTS'),
+        title: const Text('BMI CALCULATOR RESULTS'),
         actions: <Widget>[
           PopupMenuButton<String>(
             onSelected: (value) => showDialog(
               context: context,
               builder: (context) => AlertDialog(
-                title: Text('BMI Categories'),
+                title: const Text('BMI Categories'),
                 content: ListView(
                   children: getBMICategories().entries.map((entry) {
                     return ListTile(
@@ -102,7 +104,7 @@ class ResultsPage extends StatelessWidget {
                 ),
                 actions: <Widget>[
                   TextButton(
-                    child: Text('Close'),
+                    child: const Text('Close'),
                     onPressed: () {
                       Navigator.of(context).pop();
                     },
@@ -125,32 +127,63 @@ class ResultsPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'Your BMI is:',
               style: TextStyle(fontSize: 24),
             ),
             Text(
               bmi.toStringAsFixed(1),
-              style: TextStyle(fontSize: 48),
+              style: const TextStyle(fontSize: 48),
             ),
             Text(
               categoryText,
-              style: TextStyle(fontSize: 22),
+              style: const TextStyle(fontSize: 22),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
+              onPressed: () async {
                 FirebaseAuth auth = FirebaseAuth.instance;
                 if (auth.currentUser == null) {
                   Navigator.pushNamed(context, '/login');
                 } else {
-                  // Save the BMI...
+                  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+                  // Retrieve the logged-in user's ID
+                  String userId = auth.currentUser!.uid;
+
+                  // Create a new data entry with the BMI result and inputs
+                  DocumentReference bmiDocRef =
+                      firestore.collection('bmiResults').doc();
+
+                  await bmiDocRef.set({
+                    'userId': userId,
+                    'bmi': bmi,
+                    'timestamp': DateTime.now().toString(),
+                  });
+
+                  // Show a success message to the user
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Success'),
+                        content: Text('BMI result saved successfully.'),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('OK'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
                 }
               },
-              child: Text('Save'),
+              child: const Text('Save'),
               style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(
-                    const Size(200, 50)), // Ändert die Größe des Buttons
+                minimumSize: MaterialStateProperty.all(const Size(200, 50)),
               ),
             ),
           ],
